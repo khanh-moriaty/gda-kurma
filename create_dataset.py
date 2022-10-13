bb48208e6e71c4f379a176272e7e81c68a144ab2
Khanh Ho <19520624@gm.it.edu.vn>
2022-10-13 16:17:20 +0000

    implement multiprocessing for dataset prep

--- create_dataset.py
+++ create_dataset.py
@@ -1,21 +1,41 @@
 
 from PIL import Image
 import os, sys
-from shutil import copyfile
+from shutil import copyfile, rmtree
 import numpy as np
 import datasets
+import tqdm
+import istarmap
+from multiprocessing import Pool
+
+BASE_DIR = sys.argv[1]
+
+def resize_image(file_path, dest_path, size):
+    im = Image.open(file_path)
+    imResize = im.resize((size,size), Image.ANTIALIAS)
+    imResize.save(dest_path, 'PNG')
 
 # Resize images.
-def resize(path, size=64):
+def resize(path, dest, size=64):
     dirs = os.listdir(path)
-    for item in dirs:
-        if os.path.isfile(path+item):
-            im = Image.open(path+item)
-            f, e = os.path.splitext(path+item)
-            imResize = im.resize((size,size), Image.ANTIALIAS)
-            imResize.save(f + '.png', 'PNG')
+    os.makedirs(dest, exist_ok=True)
+    print('Listing', path)
+    jobs = []
+    for item in tqdm.tqdm(dirs):
+        file_path = os.path.join(path, item)
+        if os.path.isfile(file_path):
+            f, e = os.path.splitext(item)
+            dest_path = os.path.join(dest, f + '.png')
+            jobs.append((file_path, dest_path, size))
+    print('Processing', path)
+    with Pool(16) as pool:
+        for _ in tqdm.tqdm(pool.istarmap(resize_image, jobs), total=len(jobs)):
+            pass
 
-for folder in ['./dataset_32x32/M/', './dataset_32x32/F/']:
-    resize(folder, size=32)
+for folder in ['M/', 'F/']:
+    resize(os.path.join(BASE_DIR, folder), os.path.join('./tmp', folder), size=32)
 
-datasets.save_data(data_dir='dataset_32x32', save_file='dataset_32x32.mat', target_size=(32,32))
+datasets.save_data(data_dir='./tmp', save_file='dataset_32x32.mat', target_size=(32,32))
+
+print('Deleting tmp dataset dir')
+rmtree('./tmp')
